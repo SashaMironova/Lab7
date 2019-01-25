a66ad440a34b584610b8e6576689b2fa9e6815c5
SashaMironova <sasha.sanny@gmail.com>
2019-01-25 23:07:36 +0300

    First commit

---
+++ src/Client/Client.java
@@ -0,0 +1,78 @@
+package Client;
+
+import java.lang.reflect.Type;
+import java.net.*;
+import java.io.*;
+import java.util.*;
+
+import Server.InjuredPoliceman;
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+public class Client {
+    public static void main(String[] args) {
+        int serverPort = 1234;
+        String address = "127.0.0.1";
+        try {
+            InetAddress ipAddress = InetAddress.getByName(address);
+            Socket socket = new Socket(ipAddress, serverPort);
+            System.out.println("Соединение установлено\nВведите одну из команд в следующих форматах:" +
+                    "\nremove_lower номер_элемента, remove id_элемента, clear, add имя год_рождения местонахождение " +
+                    "состояние раненая_часть_лица раненая_часть_тела цвет, get_collection, select раненая_часть_лица, exit");
+
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(inputStream);
+            DataOutputStream out = new DataOutputStream(outputStream);//todo если коллекция пуста, вызвана команда гет коллекшн, что происходит
+
+            BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+            String enterStr = null;
+
+
+            Gson gson = new Gson();
+            String str; // переменная для хранения того, что прислал сервер
+
+            Type type = new TypeToken<ArrayList<InjuredPoliceman>>() {//todo сделать обработку временной недоступности сервера
+            }.getType();
+
+            while (true) {
+                enterStr = reader.readLine(); // ждем пока пользователь введет что-то и нажмет кнопку Enter.
+                if (enterStr.equals("exit")){
+                    out.close();
+                    in.close();
+                    socket.close();
+                    reader.close();
+                    System.exit(0);
+                    break;
+                }
+
+                out.writeUTF(enterStr);
+                str = in.readUTF();
+                if (str.equals("OK") || str.equals("Полицейских с такой раненой частью лица нет") || str.equals("Неправильная команда")) {
+                    System.out.println(str);
+                }
+                else{
+                    ArrayList<InjuredPoliceman> injuredPolicemen = gson.fromJson(str, type);
+                    for (int i = 0; i < injuredPolicemen.size(); i++) {
+                        System.out.println("Раненого полицейского зовут: " + injuredPolicemen.get(i).name + ", он родился в " + injuredPolicemen.get(i).yearOfBirth
+                                + " году, он находится " + injuredPolicemen.get(i).location + ", его создали " + injuredPolicemen.get(i).dateOfCreation + ", он " + injuredPolicemen.get(i).colour + " цвета");
+                    }
+                }
+                out.flush(); // заставляем поток закончить передачу данных.
+            }
+        } catch (Exception x) {
+            System.out.println("Сервер недоступен");
+        }
+    }
+}
+
+
+// TODO: Коллекцию из ЛР №5 заменить на ее потокобезопасный аналог. +
+//Операции обработки объектов коллекции должны быть реализованы с помощью Stream API с использованием лямбда-выражений.+
+//Объекты между клиентом и сервером должны передаваться в сериализованном виде.+
+//Объекты в коллекции, передаваемой клиенту, должны быть отсортированы по названию.+
+//Получив запрос, сервер должен создавать отдельный поток, который должен формировать и отправлять ответ клиенту.+
+//Клиент должен корректно обрабатывать временную недоступность сервера.
+//Обмен данными между клиентом и сервером должен осуществляться по протоколу TCP.+
+//На стороне сервера должен использоваться сетевой канал а на стороне клиента - потоки ввода-вывода.+
---
+++ src/Server/Server.java
@@ -0,0 +1,242 @@
+package Server;
+
+import com.google.gson.Gson;
+import com.google.gson.reflect.TypeToken;
+
+import java.io.*;
+import java.lang.reflect.Type;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
+
+
+public class Server {
+    private static CopyOnWriteArrayList<InjuredPoliceman> injuredPolicemen;
+
+    public static void main(String[] args) {
+
+        Gson gson = new Gson();
+        String json = "";
+
+        InputOutput inputOutput = new InputOutput();
+
+        String str = inputOutput.input();
+        Type type = new TypeToken<CopyOnWriteArrayList<InjuredPoliceman>>() {
+        }.getType();
+        injuredPolicemen = gson.fromJson(str, type);
+
+        if (injuredPolicemen == null)
+            injuredPolicemen = new CopyOnWriteArrayList<InjuredPoliceman>();
+
+        Collections.sort(injuredPolicemen, new InjuredPolicemanComparator());
+
+        int port = 1234;
+        try {
+            ServerSocket ss;
+            Socket socket;
+            ss = new ServerSocket(port);
+            System.out.println("Ожидаем поключения");
+            while (true) {
+                socket = ss.accept();
+                Socket s = socket;
+                System.out.println("Клиент подключился");
+
+                new Thread(() -> connectClient(s)).start();
+            }
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+    private static void connectClient(Socket socket) {
+        Gson gson = new Gson();
+        String json = "";
+
+        InputOutput inputOutput = new InputOutput();
+
+        try {
+            InputStream inputStream = socket.getInputStream();
+            OutputStream outputStream = socket.getOutputStream();
+
+            DataInputStream in = new DataInputStream(inputStream);
+            DataOutputStream out = new DataOutputStream(outputStream);
+            while (true) {
+                String line = null;
+                line = in.readUTF();//в лайне лежит строка, полученная от клиента
+                String[] fewWords = line.split(" ");
+                int number = 0;
+                switch (fewWords[0]) {
+
+                    case "remove_lower"://удаляет все элементы меньше введенного по индексу
+                        try {
+                            number = Integer.parseInt(fewWords[1]);
+                            for (int i = 0; i < number; i++)
+                                injuredPolicemen.remove(0);
+                            out.writeUTF("OK");
+                        } catch (Exception e) {
+                            out.writeUTF("Неправильная команда");
+                        }
+                        break;
+
+                    case "remove":
+                        try {
+                            number = Integer.parseInt(fewWords[1]);//нужно ли реализовывать обработку команды с несуществующим idse
+                            if (number != 0) {
+                                for (int j = 0; j < injuredPolicemen.size(); j++) {
+                                    if (injuredPolicemen.get(j).id == number) {
+                                        injuredPolicemen.remove(j);
+                                    }
+                                }
+                            }
+                            out.writeUTF("OK");
+                        } catch (Exception e) {
+                            out.writeUTF("Неправильная команда");
+                        }
+                        break;
+
+                    case "clear":
+                        injuredPolicemen = new CopyOnWriteArrayList<InjuredPoliceman>();
+                        out.writeUTF("OK");
+                        break;
+
+                    case "get_collection"://out.writeUTF(line)
+                        Collections.sort(injuredPolicemen, new InjuredPolicemanComparator());
+                        json = gson.toJson(injuredPolicemen);
+                        System.out.println(json);
+                        out.writeUTF(json);
+                        break;
+
+                    case "select":
+                        final AtomicReference<FacePartEnum> selectedFacePart = new AtomicReference<>(); //первая часть работующего стрим апи
+                        fewWords[1] = fewWords[1].toUpperCase();
+                        switch (fewWords[1]) {
+                            case "FOREHEAD":
+                                selectedFacePart.set(FacePartEnum.FOREHEAD);
+                                break;
+                            case "NOSE":
+                                selectedFacePart.set(FacePartEnum.NOSE);
+                                break;
+                            case "RIGHT_EYE":
+                                selectedFacePart.set(FacePartEnum.RIGHT_EYE);
+                                break;
+                            case "LEFT_EYE":
+                                selectedFacePart.set(FacePartEnum.LEFT_EYE);
+                                break;
+                            case "MOUTH":
+                                selectedFacePart.set(FacePartEnum.MOUTH);
+                                break;
+                            case "CHEEKS":
+                                selectedFacePart.set(FacePartEnum.CHEEKS);
+                                break;
+                        }
+                        List<InjuredPoliceman> policemenList = injuredPolicemen.stream().filter(o ->
+                                {
+                                    return o.injuredFacePart == selectedFacePart.get();
+                                }
+                        ).collect(Collectors.toList());
+                        if (policemenList.size() != 0) {
+                            json = gson.toJson(policemenList);
+                        } else {
+                            json = "Полицейских с такой раненой частью лица нет";
+                        }
+                        out.writeUTF(json);
+                        break;
+
+                    case "add":
+                        InjuredPoliceman injuredPoliceman = new InjuredPoliceman();
+                        injuredPoliceman.name = fewWords[1];
+                        injuredPoliceman.yearOfBirth = Integer.parseInt(fewWords[2]);
+                        injuredPoliceman.location = fewWords[3];
+                        fewWords[4] = fewWords[4].toUpperCase();
+                        switch (fewWords[4]) {
+                            case "NAUSEA":
+                                injuredPoliceman.stateEnum = StateEnum.NAUSEA;
+                                break;
+                            case "SMOOTH":
+                                injuredPoliceman.stateEnum = StateEnum.SMOOTH;
+                                break;
+                            case "GOOD":
+                                injuredPoliceman.stateEnum = StateEnum.GOOD;
+                                break;
+                            case "BAD":
+                                injuredPoliceman.stateEnum = StateEnum.BAD;
+                                break;
+                            default:
+                                out.writeUTF("Неправильная команда");
+                                break;
+                        }
+                        injuredPolicemen.add(injuredPoliceman);
+                        fewWords[5] = fewWords[5].toUpperCase();
+                        switch (fewWords[5]) {
+                            case "FOREHEAD":
+                                injuredPoliceman.injuredFacePart = FacePartEnum.FOREHEAD;
+                                break;
+                            case "NOSE":
+                                injuredPoliceman.injuredFacePart = FacePartEnum.NOSE;
+                                break;
+                            case "RIGHT_EYE":
+                                injuredPoliceman.injuredFacePart = FacePartEnum.RIGHT_EYE;
+                                break;
+                            case "LEFT_EYE":
+                                injuredPoliceman.injuredFacePart = FacePartEnum.LEFT_EYE;
+                                break;
+                            case "MOUTH":
+                                injuredPoliceman.injuredFacePart = FacePartEnum.MOUTH;
+                                break;
+                            case "CHEEKS":
+                                injuredPoliceman.injuredFacePart = FacePartEnum.CHEEKS;
+                                break;
+                            case "":
+                                break;
+                            default:
+                                out.writeUTF("Неправильная команда");
+                                break;
+                        }
+                        fewWords[6] = fewWords[6].toUpperCase();
+                        switch (fewWords[6]) {
+                            case "LEFT_HAND":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.LEFT_HAND);
+                                break;
+                            case "RIGHT_HAND":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.RIGHT_HAND);
+                                break;
+                            case "LEFT_LEG":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.LEFT_LEG);
+                                break;
+                            case "RIGHT_LEG":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.RIGHT_LEG);
+                                break;
+                            case "HEAD":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.HEAD);
+                                break;
+                            case "RIGHT_FEET":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.RIGHT_FEET);
+                                break;
+                            case "LEFT_FEET":
+                                injuredPoliceman.bodyPartsEnum.add(BodyPartEnum.LEFT_FEET);
+                                break;
+                            default:
+                                out.writeUTF("Неправильная команда");
+                                break;
+                        }
+                        injuredPoliceman.colour = fewWords[7];
+                        json = gson.toJson(injuredPolicemen);
+                        inputOutput.output(json);
+                        out.writeUTF("OK");
+                        break;
+
+                    default:
+                        out.writeUTF("Неправильная команда");
+                        break;
+                }
+            }
+
+        } catch (Exception e) {
+            System.out.println("Клиент отключился");
+        }
+    }
+}
